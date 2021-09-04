32ac6b74f8df920f42718af77db7f033803a485a
Yashshah-raj <yshah3642@gmail.com>
2021-09-04 18:53:53 +0530

    second push

---
+++ src/main/java/navkar/jewels/com/navkar/resources/rest/LoginController.java
@@ -0,0 +1,29 @@
+package navkar.jewels.com.navkar.resources.rest;
+
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.social.connect.Connection;
+import org.springframework.social.facebook.api.Facebook;
+import org.springframework.social.facebook.api.User;
+import org.springframework.social.facebook.connect.FacebookConnectionFactory;
+import org.springframework.social.oauth2.AccessGrant;
+import org.springframework.social.oauth2.OAuth2Operations;
+import org.springframework.social.oauth2.OAuth2Parameters;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin
+public class LoginController {
+
+	
+	public String LoginControllers() {
+		return "Hello World";
+	}
+	
+}
