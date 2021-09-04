a3fd92ebd7dcce2f1e6b4fe7cd91aa1fe63fc353
Yashshah-raj <57977247+Yashshah-raj@users.noreply.github.com>
2021-09-04 18:40:51 +0530

    Update PatientDetailsRepo.java

--- navkar/src/main/java/navkar/jewels/com/navkar/repository/PatientDetailsRepo.java
+++ navkar/src/main/java/navkar/jewels/com/navkar/repository/PatientDetailsRepo.java
@@ -1,9 +1,1 @@
-package navkar.jewels.com.navkar.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import navkar.jewels.com.navkar.domain.PatientDetails;
-
-public interface PatientDetailsRepo extends JpaRepository<PatientDetails, Integer> {
-
-}
