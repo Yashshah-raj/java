6ab5def16b0e315d3c9231f12265479a8b3d3de4
Yashshah-raj <57977247+Yashshah-raj@users.noreply.github.com>
2021-09-04 19:27:22 +0530

    Update NavkarApplicationTests.java

--- navkar/src/test/java/navkar/jewels/com/navkar/NavkarApplicationTests.java
+++ navkar/src/test/java/navkar/jewels/com/navkar/NavkarApplicationTests.java
@@ -3,7 +3,7 @@ package navkar.jewels.com.navkar;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class NavkarApplicationTests {
 
 	@Test
