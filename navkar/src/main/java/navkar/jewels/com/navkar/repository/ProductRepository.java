c2707602a0465caa0682095a70d81db22ca7f090
Yashshah-raj <57977247+Yashshah-raj@users.noreply.github.com>
2021-09-04 18:41:13 +0530

    Update ProductRepository.java

--- navkar/src/main/java/navkar/jewels/com/navkar/repository/ProductRepository.java
+++ navkar/src/main/java/navkar/jewels/com/navkar/repository/ProductRepository.java
@@ -1,9 +1,1 @@
-package navkar.jewels.com.navkar.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import navkar.jewels.com.navkar.domain.Product;
-
-public interface ProductRepository extends JpaRepository<Product,Integer> {
-
-}
